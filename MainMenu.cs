3a55bfa60948881277e5a01eeeaa78f426be05e4
tylercaldwell27 <44902413+tylercaldwell27@users.noreply.github.com>
2019-01-22 20:26:19 -0500

    Create MainMenu.cs

---
+++ MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    
+    void Update() {
+        //checks what scene its in
+        if (Input.GetButton("play")&& SceneManager.GetActiveScene().buildIndex == 0) {//when your on the mainmenu and p is pressed
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);// goes to the next scene
+        }
+        if (Input.GetButton("play") && SceneManager.GetActiveScene().buildIndex == 2)//when your on the mainmenu and p is pressed
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);// goes to the next scene
+        }
+    }
+    
+}
